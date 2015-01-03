2a28c1c4366c3143e46238d9d4ad619e2d2d657e
lrvick <lance@lrvick.net>
2015-01-03 06:32:36 -0500

    dynamic server NUMERIC and HOSTNAME

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # do some magic here to pull in opers.conf from an EBS volume or something
-#if [ -z "$TOKEN" ]; then
-#  echo "Missing \$TOKEN"
-#  exit 1
-#fi
+if [ -z "$NUMERIC" ]; then
+  echo "Missing \$NUMERIC"
+  exit 1
+fi
+if [ -z "$HOSTNAME" ]; then
+  echo "Missing \$HOSTNAME"
+  exit 1
+fi
 
-#sed -i "s/TOKEN/$TOKEN/" /opt/unrealircd/.conf
+sed -i "s/_NUMERIC_/$TOKEN/" /opt/unrealircd/unrealircd.conf
+sed -i "s/_HOSTNAME_/$HOSTNAME/" /opt/unrealircd/unrealircd.conf
 
 /opt/unrealircd/src/ircd
 tail -f /var/log/ircd.log
