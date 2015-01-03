645956cbb9bb6485f1a0ae20595e1655872671de
lrvick <lance@lrvick.net>
2015-01-03 07:18:21 -0500

    dynamic server linking via env vars

--- run.sh
+++ run.sh
@@ -9,9 +9,24 @@ if [ -z "$HOSTNAME" ]; then
   echo "Missing \$HOSTNAME"
   exit 1
 fi
+if [ -z "$LINK1_HOST" ]; then
+  echo "Missing \$LINK1_HOST"
+  exit 1
+fi
+if [ -z "$LINK1_PASSWORD_RECEIVE" ]; then
+  echo "Missing \$LINK1_PASSWORD_RECEIVE"
+  exit 1
+fi
+if [ -z "$LINK1_PASSWORD_CONNECT" ]; then
+  echo "Missing \$LINK1_PASSWORD_CONNECT"
+  exit 1
+fi
 
 sed -i "s/_NUMERIC_/$NUMERIC/" /opt/unrealircd/unrealircd.conf
 sed -i "s/_HOSTNAME_/$HOSTNAME/" /opt/unrealircd/unrealircd.conf
+sed -i "s/_LINK1_HOST_/$LINK1_HOST/" /opt/unrealircd/unrealircd.conf
+sed -i "s/_LINK1_PASSWORD_CONNECT_/$LINK1_PASSWORD_CONNECT/" /opt/unrealircd/unrealircd.conf
+sed -i "s/_LINK1_PASSWORD_RECEIVE_/$LINK1_PASSWORD_RECEIVE/" /opt/unrealircd/unrealircd.conf
 
 /opt/unrealircd/src/ircd
 tail -f /var/log/ircd.log
