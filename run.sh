8773fd9c34387377432475313dc4942be7a642e9
lrvick <lance@lrvick.net>
2015-01-05 23:11:01 -0500

    add link name

--- run.sh
+++ run.sh
@@ -9,6 +9,10 @@ if [ -z "$HOSTNAME" ]; then
   echo "Missing \$HOSTNAME"
   exit 1
 fi
+if [ -z "$LINK1_NAME" ]; then
+  echo "Missing \$LINK1_HOST"
+  exit 1
+fi
 if [ -z "$LINK1_HOST" ]; then
   echo "Missing \$LINK1_HOST"
   exit 1
@@ -36,6 +40,7 @@ fi
 
 sed -i "s/_NUMERIC_/$NUMERIC/" /opt/unrealircd/unrealircd.conf
 sed -i "s/_HOSTNAME_/$HOSTNAME/" /opt/unrealircd/unrealircd.conf
+sed -i "s/_LINK1_NAME_/$LINK1_HOST/" /opt/unrealircd/unrealircd.conf
 sed -i "s/_LINK1_HOST_/$LINK1_HOST/" /opt/unrealircd/unrealircd.conf
 sed -i "s/_LINK1_PASSWORD_CONNECT_/$LINK1_PASSWORD_CONNECT/" /opt/unrealircd/unrealircd.conf
 sed -i "s/_LINK1_PASSWORD_RECEIVE_/$LINK1_PASSWORD_RECEIVE/" /opt/unrealircd/unrealircd.conf
